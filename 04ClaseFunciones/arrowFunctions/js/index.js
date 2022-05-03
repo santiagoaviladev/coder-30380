2bdb213e8486e379a240f63db63647411bbf6274
Santiago Avila <xavier0823@gmail.com>
2022-05-03 16:49:39 -0500

    Template Functions

---
+++ 04ClaseFunciones/arrowFunctions/js/index.js
@@ -0,0 +1,11 @@
+//Scope Global 
+
+
+
+
+// Scope local
+
+
+
+
+//let y const vs var
---
+++ 04ClaseFunciones/ejercicio02/index.js
@@ -0,0 +1,31 @@
+
+
+ /* 
+ 
+    Escriba un programa que le pida al usuario su nombre y  
+    muestre un menú de opciones:
+
+    1. PEDIR CAFÉ  
+    2. PEDIR MATE 
+    3. FIN
+
+
+    1. Si el usuario pide café o mate, debe mostrar un mensaje para 
+    que el usuario ingrese un monto de dinero, 
+    mostrando el precio del producto elegido
+    La máquina solo recibe billetes de 1, 5 y 10 usd
+    El precio del café es de 2 usd. 
+    El precio del mate es de 3 usd. 
+
+    2. Si el monto ingresado es inferior al costo del producto,
+    se debe mostrar un mensaje solicitando más dinero. 
+
+    3. Si el monto ingresado es superior al costo del producto, 
+    debe imprimir las vueltas y dar las gracias
+
+    4. Si el usuario no ingresa un monto aceptado, 
+    debe mostrar un mensaje diciendo que devuelve el dinero. 
+
+    *** DEBE USAR FUNCIONES PARA REALIZAR EL EJERCICIO ***
+
+ */
