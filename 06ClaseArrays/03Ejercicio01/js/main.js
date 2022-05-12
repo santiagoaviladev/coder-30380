caa550850c0a16b64dfc4dc2959f7afc88dd2152
Santiago Avila <xavier0823@gmail.com>
2022-05-12 17:57:57 -0500

    Ejercicio 01 resuelto

--- 06ClaseArrays/03Ejercicio01/js/main.js
+++ 06ClaseArrays/03Ejercicio01/js/main.js
@@ -4,4 +4,28 @@
   // imprimir la suma de los elementos del array
   // imprimir la multipliación de los elementos del array
 
-  
+  let contador=0;
+  let miArray = [];
+  while(contador<5)
+  {
+      let numero = Number(prompt("Ingrese un numero"));
+      miArray.push(numero);
+      contador++;
+  }
+  console.log("IMPRIMIR ARRAY");
+  console.log(miArray);
+
+  let suma = 0;
+  for(let i=0;i<miArray.length;i++)
+  {
+    suma += miArray[i];
+  }
+  console.log("SUMA", suma);
+
+  let multiplicacion = 1;
+  for(let i=0;i<miArray.length;i++)
+  {
+    //multiplicacion=multiplicacion*miArray[i];
+    multiplicacion*=miArray[i];
+  }
+  console.log("MUL", multiplicacion);
