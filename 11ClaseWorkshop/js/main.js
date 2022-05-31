2e7d46e8e4471a3f3cf562a756cad4ab7193fe74
Santiago Avila <xavier0823@gmail.com>
2022-05-31 11:08:11 -0500

    template workshop

---
+++ 11ClaseWorkshop/js/main.js
@@ -0,0 +1,26 @@
+
+/*
+    Parte 1:
+    El archivo 'data.js' tiene una copia de la base de datos de Rick & Morty
+    1. Cree una clase 'Personaje' 
+                        id
+                        name
+                        image
+                        OtherInfo:{
+                            gender
+                            status
+                            location name
+                        }
+    2. Agregue todos los elementos de data a un array con elementos de tipo Personaje
+    3. Cree una funcion que despliegue tarjetas con los personajes de Rick y Morty en la pantalla. 
+        La tarjeta debe mostrar la imagen y el nombre del personaje
+    4. Cuando se haga click en la tarjeta, se debe mostrar 'el reverso' de la tarjeta con la 
+        información contenida en Other Info (Y visceversa)
+
+    Parte 2: 
+    5. Agregue una clase, para manejar un carrito de compras de tarjetas de Ricky y Morty 
+    6. Modifique el programa, para agregar la acción de compra 
+    7. Agregue una función que muestre 'El total comprado' 
+
+
+*/
