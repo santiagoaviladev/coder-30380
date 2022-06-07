aa585583e16e0de33772ee5743b92f156fbf8c4b
Santiago Avila <xavier0823@gmail.com>
2022-06-07 17:57:24 -0500

    Sweet Alert

--- 12ClaseOperadoresAvanzados/12SpreadOperator/js/main.js
+++ 12ClaseOperadoresAvanzados/12SpreadOperator/js/main.js
@@ -1,4 +1,23 @@
 
+/*Spread en array*/ 
 const mascotas = ["firulais", "coco", "sarna", "vainilla", "garrafa", "cerveza", "comotu"];
+console.log(...mascotas);
+ console.log("hola", "esto", "es", "una", "cadena");
 
- console.log(...mascotas);
+ const numeros = [1,200,4000,700,800];
+
+ console.log(Math.max(...numeros));
+
+/*Spread en objetos*/
+ let usuario = {id:1,nombre:"Juan", apellido:"Ferreto"}
+ console.log(usuario);
+
+ let usuario2 = {...usuario};
+ usuario2.nombre= "Pepito";
+ console.log("USUARIO2", usuario2);
+ console.log("USUARIO",usuario);
+
+ let usuario3 = {...usuario, nombre: "JUANITO", rol:"Estudiante"};
+ console.log(usuario3);
+
+ 
--- 13ClaseLibrerias/01SWEETALERT/js/main.js
+++ 13ClaseLibrerias/01SWEETALERT/js/main.js
@@ -4,10 +4,80 @@ init();
 function init()
 {
     saludarConSweetAlert();
-    /*agregarEventoABoton();
+    
     alertSinBotonDeConfirmacion();
     alertConImagenes();
-    alertConEventos();*/
+    alertConEventos();
 
 }
 
+function saludarConSweetAlert()
+{
+    const btn = document.getElementById("btnSweet");
+    btn.addEventListener("click", ()=>{
+
+        Swal.fire({
+            title:'Hola',
+            text:'Esto es una prueba',
+            icon: 'success',
+            confirmButtonText: 'ESTÁ BIEN'
+        })
+    })
+
+    
+}
+
+function alertSinBotonDeConfirmacion()
+{
+    const btn = document.getElementById("btnSweet2");
+    btn.addEventListener("click", ()=>{
+
+        Swal.fire({
+            title:'Hola',
+            text:'Esto es una prueba',
+            icon: 'success',
+            showConfirmButton: false,
+        })
+    })
+}
+
+function alertConImagenes()
+{
+    const btn = document.getElementById("btnSweet3");
+    btn.addEventListener("click", ()=>{
+
+        Swal.fire({
+            title:'Hola',
+            text:'Esto es una prueba',
+            icon: 'success',
+            showConfirmButton: false,
+            imageWidth: "50%",
+            imageUrl: 'https://assets.stickpng.com/images/580b57fcd9996e24bc43c325.png'
+        })
+    })
+}
+
+function alertConEventos()
+{
+    const btn = document.getElementById("btnSweet4");
+    btn.addEventListener("click", ()=>{
+
+        Swal.fire({
+            title:'Hola',
+            text:'Esto es una prueba',
+            icon: 'success',
+            confirmButtonText: 'ESTÁ BIEN',
+            cancelButtonText: 'No ESTÁ BIEN',
+            showCancelButton: true,
+        }).then((result)=>{
+            if(result.isConfirmed)
+            {
+                alert("CONFIRMADO");
+            }
+            else
+            {
+                alert("NO CONFIRMADO");
+            }
+        })
+    })
+}
