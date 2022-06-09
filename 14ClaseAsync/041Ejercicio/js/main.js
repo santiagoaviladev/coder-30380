e72cdb80d4e50d8109136d9bcdd4fa159a4ab706
Santiago Avila <xavier0823@gmail.com>
2022-06-09 18:59:30 -0500

    clase resuelto

--- 14ClaseAsync/041Ejercicio/js/main.js
+++ 14ClaseAsync/041Ejercicio/js/main.js
@@ -1,7 +1,7 @@
 /*
  Simule la petición de datos en una promesa, del array de usuarios definido.
  1. Pida al usuario que ingrese un valor (1 o 0)
-    Si el usuario ingresa 1, la simulación debe aceptar la petición
+    Si el usuario ingresa 1, la simulación debe aceptar la petición y retornar el array
     Si el uusario ingresa 0, la simulación debe rechazar la petición 
     Al final, debe mostrar un mensaje: 'Gracias por usar nuestro servicio' 
 */
@@ -14,6 +14,26 @@ const usuarios = [
     {id:5, nombre:'María', apellido:'Ortíz'},
 ]
 
-const pedirUsuarios = ()=>{
-    
-}
+
+const pedirUsuarios = (res) => new Promise((resolve, reject)=>{
+
+    if(res===true)
+    {
+        resolve(usuarios)
+    }
+    else
+    {
+        reject("ERROR INDUCIDO POR MI: FALLÓ")
+    }
+
+})
+
+ 
+
+  let resultado = prompt("Indique qué quiere que pase")
+  let myRes = resultado === "1" ? true : false;
+
+  pedirUsuarios(myRes)
+  .then((response)=>console.log(response))
+  .catch((error)=>console.log(error))
+  .finally(()=>console.log("FINALIZAMOS"));
