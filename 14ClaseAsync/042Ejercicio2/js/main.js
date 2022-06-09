8afdf5053a1360faa7fb67c433c6f65c8cb19967
Santiago Avila <xavier0823@gmail.com>
2022-06-09 17:49:27 -0500

    Ejercicio Explotar Pnatalla

--- 14ClaseAsync/042Ejercicio2/js/main.js
+++ 14ClaseAsync/042Ejercicio2/js/main.js
@@ -1,5 +1,28 @@
 /*
  Escriba un programa que muestre el mensaje: 
- Su pantalla va a explotar en 5 segundos
+ Su pantalla va a explotar en 5 segundos 
+ y haga un conteo hacia atrás
  Y tras 5 segundos, ponga el dom, con el fondo en rojo. 
 */
+
+//Contribución de MArtín Mondino
+const msg = document.getElementById("msg")
+
+
+let contador = 5;
+
+msg.innerHTML="SU PANTALLA EXPLOTA EN" + " " + contador + " segundos";
+
+
+let interval = setInterval(()=>{
+
+    contador--;
+    msg.innerHTML="SU PANTALLA EXPLOTA EN" + " " + contador + " segundos";
+    
+    if(contador===0)
+    {
+        clearInterval(interval);
+        document.body.setAttribute("style", "background-color: red")
+    }
+
+}, 1000)
