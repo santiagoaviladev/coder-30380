605c9424ffdbfaa39349b36b2dfd7b2ce15e3eb9
Santiago Avila <xavier0823@gmail.com>
2022-06-14 17:44:08 -0500

    fecth - GET

--- 15AjaxYFETCH/js/main.js
+++ 15AjaxYFETCH/js/main.js
@@ -5,9 +5,41 @@ const mainContainer = document.getElementById("appContainer");
 function programarMenuInicial(){
 
    // programarBotonAdmin();
-   // programarBotonPreview();
+   programarBotonPreview();
 
 }
 
+function programarBotonPreview()
+{
+   const btn = document.getElementById("ver")
+   btn.addEventListener("click", ()=>{
+      cargarPosts();
+   })
+}
 
+function cargarPosts()
+{
+   fetch("https://jsonplaceholder.typicode.com/posts/")
+   .then((response)=>response.json())
+   .then((json)=>mostrarDatos(json))
+   .catch(()=>alert("intente de nuevo"))
+}
 
+function mostrarDatos(data)
+{
+   const div = document.getElementById("appContainer");
+   data.forEach(blogPost=>{
+
+      const {title, body} = blogPost
+
+      const divPost = document.createElement("div");
+      divPost.innerHTML=`<h2>${title}</h2>
+                          <p>
+                           ${body}
+                          </p>
+                          <hr />
+      `
+      div.appendChild(divPost)
+
+   })
+}
