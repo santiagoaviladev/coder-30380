53a949a02ab4f656cbca2c982fc510d17bc06428
Santiago Avila <xavier0823@gmail.com>
2022-06-13 21:08:45 -0500

    Blog fetch

---
+++ 15AjaxYFETCH/js/main.js
@@ -0,0 +1,10 @@
+programarMenuInicial();
+
+const mainContainer = document.getElementById("appContainer");
+
+function programarMenuInicial(){
+
+    programarBotonAdmin();
+    programarBotonPreview();
+
+}
